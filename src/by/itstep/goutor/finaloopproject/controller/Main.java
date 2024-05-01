e2119dbe76d5db729ce244cb9e650604c4887ce4
nikishkaa <nikita.goutor@gmail.com>
2024-05-01 19:31:35 +0300

    Add Creator. Update main class. Update Logger.

--- src/by/itstep/goutor/finaloopproject/controller/Main.java
+++ src/by/itstep/goutor/finaloopproject/controller/Main.java
@@ -2,6 +2,7 @@ package by.itstep.goutor.finaloopproject.controller;
 
 import by.itstep.goutor.finaloopproject.model.entity.container.Group;
 import by.itstep.goutor.finaloopproject.model.entity.npc.*;
+import by.itstep.goutor.finaloopproject.util.Creator;
 import by.itstep.goutor.finaloopproject.util.UserUI;
 import by.itstep.goutor.finaloopproject.view.Printer;
 import org.apache.log4j.BasicConfigurator;
@@ -18,30 +19,15 @@ public class Main {
     public static void main(String[] args) {
         BasicConfigurator.configure();
         LOGGER.setLevel(Level.DEBUG);
-        Archer archer1 = new Archer("123", true, 100,
-                100, 100, 10, 10, 10
-                , "123", 2, 2, 2);
 
 
-        Knight knight1 = new Knight("123", true, 100,
-                100, 100, 10, 10, 10
-                , "123", 2, 2, 3);
+        Group heroGroup = Creator.getHeroGroup();
+        Group bossGroup = Creator.getBossGroup();
 
 
-        Dragon dragon1 = new Dragon("123", true, 100,
-                100, 100, 100, 100, 100.0);
-
-        Hydra hydra1 = new Hydra("123", true, 100,
-                100, 100, 100, 3);
-
-        NPC[] npc = new NPC[]{archer1, knight1, dragon1, hydra1};
-        Group group = new Group(npc);
-
-        Printer.print(group.toString());
+        Printer.print(heroGroup.toString() + bossGroup);
         Printer.print(UserUI.getUserUI());
 
-        LOGGER.info(group.toString());
         LOGGER.info(UserUI.getUserUI());
-
     }
 }
---
+++ src/by/itstep/goutor/finaloopproject/util/Creator.java
@@ -0,0 +1,42 @@
+package by.itstep.goutor.finaloopproject.util;
+
+import by.itstep.goutor.finaloopproject.model.entity.container.Group;
+import by.itstep.goutor.finaloopproject.model.entity.npc.*;
+
+import static by.itstep.goutor.finaloopproject.controller.Main.LOGGER;
+
+public class Creator {
+    public static Group getHeroGroup() {
+        Archer archer1 = new Archer("123", true, 100,
+                100, 100, 10, 10, 10
+                , "123", 2, 2, 2);
+
+
+        Knight knight1 = new Knight("123", true, 100,
+                100, 100, 10, 10, 10
+                , "123", 2, 2, 3);
+
+        NPC[] npcGroup = new NPC[]{archer1, knight1};
+
+        Group heroGroup = new Group(npcGroup);
+        LOGGER.info(heroGroup);
+
+        return heroGroup;
+    }
+
+    public static Group getBossGroup() {
+        Dragon dragon1 = new Dragon("123", true, 100,
+                100, 100, 100, 100, 100.0);
+
+        Hydra hydra1 = new Hydra("123", true, 100,
+                100, 100, 100, 3);
+
+        NPC[] npcGroup = new NPC[]{dragon1, hydra1};
+
+        Group bossGroup = new Group(npcGroup);
+        LOGGER.info(bossGroup);
+
+
+        return bossGroup;
+    }
+}
