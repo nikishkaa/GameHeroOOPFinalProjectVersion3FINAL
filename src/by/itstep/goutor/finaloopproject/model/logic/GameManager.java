041d1f0e927bb6d4888418f0807ea8929d35ebf8
nikishkaa <nikita.goutor@gmail.com>
2024-05-07 20:31:56 +0300

    Update logic.

--- src/by/itstep/goutor/finaloopproject/model/logic/GameManager.java
+++ src/by/itstep/goutor/finaloopproject/model/logic/GameManager.java
@@ -1,8 +1,34 @@
 package by.itstep.goutor.finaloopproject.model.logic;
 
+import by.itstep.goutor.finaloopproject.model.entity.container.Group;
+import by.itstep.goutor.finaloopproject.model.entity.npc.NPC;
+
+import static by.itstep.goutor.finaloopproject.controller.Main.LOGGER;
+
 public class GameManager {
+    public static boolean getVinHeroBoos(Group hero, Group boss) {
+        if (hero == null || boss == null || Group.DEFAULT_SIZE == 0) {
+            LOGGER.warn("hero == null || boss == null || Group.DEFAULT_SIZE == 0");
+            return false;
+        }
+        boolean vin = false;
 
+//        int heroStat = ;
+//        int bossStat = ;
 
+        return vin;
+    }
+
+    public static int getVinStat(Group group) {
+        int stat = 0;
+
+        for (NPC t : group.getGroup()) {
+            stat += t.getBaseDamage();
+//            stat += t.g
+        }
+
+        return stat;
+    }
 
 
 }
