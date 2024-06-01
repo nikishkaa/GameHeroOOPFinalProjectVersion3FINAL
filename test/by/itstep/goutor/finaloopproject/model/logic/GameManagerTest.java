b12d3f1988602cc6f5c1103597b305637c1246ae
nikishkaa <nikita.goutor@gmail.com>
2024-06-02 00:59:29 +0300

    Update log4j properties. Update Test.

--- test/by/itstep/goutor/finaloopproject/model/logic/GameManagerTest.java
+++ test/by/itstep/goutor/finaloopproject/model/logic/GameManagerTest.java
@@ -1,7 +1,25 @@
 package by.itstep.goutor.finaloopproject.model.logic;
 
+import by.itstep.goutor.finaloopproject.model.entity.container.Group;
+import by.itstep.goutor.finaloopproject.model.entity.npc.Boss;
+import by.itstep.goutor.finaloopproject.model.entity.npc.Hero;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GameManagerTest {
 
+
+    @Test
+    public void getVinHeroBoosWithStartStaffByNull() {
+        Group hero = null;
+        Group boss = null;
+        boolean expected = false;
+
+        boolean actual = GameManager.getVinHeroBoosWithStartStaff(hero, boss);
+
+        assertEquals(expected, actual);
+    }
 
 
 }
