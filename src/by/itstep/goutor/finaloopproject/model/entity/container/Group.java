a05c8634527a1e581fc5ed993e43d9071187c2ba
nikishkaa <nikita.goutor@gmail.com>
2024-06-02 18:52:21 +0300

    Update Group.

--- src/by/itstep/goutor/finaloopproject/model/entity/container/Group.java
+++ src/by/itstep/goutor/finaloopproject/model/entity/container/Group.java
@@ -5,33 +5,27 @@ import by.itstep.goutor.finaloopproject.model.entity.npc.Entity;
 public class Group {
     public static final int DEFAULT_SIZE = 10;
 
-    private Entity[] npc;
+    private  final Entity[] entity;
 
     public Group() {
-        npc = new Entity[DEFAULT_SIZE];
+        entity = new Entity[DEFAULT_SIZE];
     }
 
     public Group(Entity[] npc) {
-        this.npc = npc;
+        this.entity = npc;
     }
 
-    public int setSize() {
-        return npc.length;
-    }
 
     public Entity[] getGroup() {
-        return npc;
+        return entity;
     }
 
-    public void setGroup(Entity[] npc) {
-        this.npc = npc;
-    }
 
     @Override
     public String toString() {
         StringBuilder builder = new StringBuilder("\nList of npc\n");
 
-        for (Entity npcGroup : npc) {
+        for (Entity npcGroup : entity) {
             builder.append(npcGroup).append("\n");
         }
 
