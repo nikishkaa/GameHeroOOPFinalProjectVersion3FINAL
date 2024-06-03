e2c9b4b993cfcfe3749972ed14e3461427340805
nikishkaa <nikita.goutor@gmail.com>
2024-06-03 20:33:30 +0300

    Update GameManager.

--- src/by/itstep/goutor/finaloopproject/model/logic/GameManager.java
+++ src/by/itstep/goutor/finaloopproject/model/logic/GameManager.java
@@ -1,13 +1,19 @@
 package by.itstep.goutor.finaloopproject.model.logic;
 
+import by.itstep.goutor.finaloopproject.model.entity.artifact.Artifact;
 import by.itstep.goutor.finaloopproject.model.entity.container.Group;
 import by.itstep.goutor.finaloopproject.model.entity.npc.*;
+
+import java.util.List;
+
 
 import static by.itstep.goutor.finaloopproject.controller.Main.LOGGER;
 
 public class GameManager {
+    // Calculate win or no hero with a start staff
     public static boolean getVinHeroBoosWithStartStaff(Group hero, Group boss) {
-        if (hero == null || boss == null || Group.DEFAULT_SIZE == 0) {
+        if (hero == null || boss == null || Group.DEFAULT_SIZE == 0
+                || hero.getGroup().length == 0 || boss.getGroup().length == 0) {
             LOGGER.warn("hero == null || boss == null || Group.DEFAULT_SIZE == 0");
             return false;
         }
@@ -16,13 +22,37 @@ public class GameManager {
     }
 
 
-    // FOR TEST DELETE THIS METHOD!!!
+    // Search a min stats artifact to equip in a hero
+    private static Artifact findMinStatArtifact(List<Artifact> artifacts, Group hero, Group boss) {
+        Artifact minStatArtifactUp = null;
 
-//    public static int getVinStats(Group hero) {
-//        return getVinStat(hero);
-//    }
+        int heroVinStat = getVinStat(hero);
+        int bossVinStat = getVinStat(boss);
+
+        for (Artifact artifact : artifacts) {
+            if (heroVinStat + artifact.getArtifactDamage() > bossVinStat) {
+                if (minStatArtifactUp == null || artifact.getArtifactDamage() < minStatArtifactUp.getArtifactDamage()) {
+                    minStatArtifactUp = artifact;
+                }
+            }
+        }
+
+        return minStatArtifactUp;
+    }
 
 
+    // Getter for min artifact to win |
+    public static Artifact getMinStatArtifact(List<Artifact> artifacts, Group hero, Group boss) {
+        if (hero == null || boss == null || Group.DEFAULT_SIZE == 0
+                || hero.getGroup().length == 0 || boss.getGroup().length == 0) {
+            LOGGER.warn("hero == null || boss == null || Group.DEFAULT_SIZE == 0");
+        }
+
+        return findMinStatArtifact(artifacts, hero, boss);
+    }
+
+
+    // Calculate entity total stats
     private static int getVinStat(Group group) {
         int stat = 0;
         for (Entity t : group.getGroup()) {
@@ -56,16 +86,13 @@ public class GameManager {
                     }
                 }
 
-
             } else {
                 stat = 0;
 
             }
         }
 
-
         return stat;
     }
-
 
 }
